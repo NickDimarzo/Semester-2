e761415e9b6123f710c3287666881f3fcc338ef5
Nick Dimarzo <124927183+NickDimarzo@users.noreply.github.com>
2023-06-27 20:30:51 -0600

    Add files via upload

---
+++ lab_creating_exeptions/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace lab5_creating_exeptions
+{
+    class MainProgram
+    {
+        class Circle 
+        {
+
+            public double Radius;
+
+            public void SetRadius() 
+            {
+            
+            }
+
+        }
+
+        static void main(string[] args) 
+        {
+            
+        
+
+        }
+    }
+}
